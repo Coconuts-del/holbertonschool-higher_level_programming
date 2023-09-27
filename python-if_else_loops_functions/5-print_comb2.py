2de6692fbede1427c45f02ecd4d9d0deb4e91f9b
root <enidaucl@gmail.com>
2023-09-27 09:46:58 -0700

    5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 # prints all numbers from 00 to 99 with "," as separator
 for number in range(0, 100):
     if number < 99:
-        print("{:02}, ".format(number), end="")
+        print("{:02}".format(number), end=", ")
     else:
-        print("{}".format(number))
+        print(number)
