5348d491534ef237a3b308975abb3ff5e5cdf0cc
root <enidaucl@gmail.com>
2023-10-04 14:53:42 -0700

    create matrix square

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    return([list(map(lambda x: x ** 2, row)) for row in matrix])
+    """ or with nested loops
+    matrix_square = []
+    for i in range(len(matrix)):
+        matrix_row = []
+        for j in range(len(matrix[i])):
+            matrix_row.append(matrix[i][j] ** 2)
+        matrix_square.append(matrix_row)
+    return (matrix_square)"""
