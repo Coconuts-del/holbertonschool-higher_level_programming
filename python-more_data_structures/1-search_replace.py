50046ad920b016008f1305136e50ffab373961df
root <enidaucl@gmail.com>
2023-10-05 10:07:24 -0700

    search an replace an element

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+def search_replace(my_list, search, replace):
+    return([replace if i == search else i for i in my_list])
+    """ list_result = my_list[:]
+    for i in range(len(my_list)):
+        if list_result[i] is search:
+            list_result[i] = replace
+    return (list_result) """
