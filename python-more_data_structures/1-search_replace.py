0caedf8bdbeca836e0448e22b9b04405b6f1c936
root <enidaucl@gmail.com>
2023-10-05 11:10:41 -0700

    Pycodestyle

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return([replace if i == search else i for i in my_list])
+    return ([replace if i == search else i for i in my_list])
     """ list_result = my_list[:]
     for i in range(len(my_list)):
         if list_result[i] is search:
