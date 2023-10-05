2df7afbe5281eee32c8f727db7583549db40ff5a
root <enidaucl@gmail.com>
2023-10-05 11:14:13 -0700

    add unigue integers of a list

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    return (sum(list(set(my_list))))
+    """ list_result = []
+    for i in my_list:
+         if i not in list_result:
+              list_result.append(i)
+         return sum(list_result) """
