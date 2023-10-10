90047579744ade61e1e469d1161f53091d1f1126
root <enidaucl@gmail.com>
2023-10-10 03:06:49 -0700

    create 1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+##!/usr/bin/python3
+""" defines a class """
+
+
+class Square:
+    """ define class Square with private instance size """
+    def __init__(self, size):
+        """ size : no verification on type & value """
+        self.__size = size
