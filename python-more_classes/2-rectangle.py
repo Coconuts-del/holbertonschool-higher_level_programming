141d27249d9abd3816791a6aefde7ede893b5db6
root <enidaucl@gmail.com>
2023-10-13 02:35:37 -0700

    create 2-rectangle

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" class  defines a rectangle """
+
+
+class Rectangle:
+    """ define class Rectangle with private instance size """
+    def __init__(self, width=0, height=0):
+        """
+        width : width of rectangle
+        height : height of rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ retrieve width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ width : set and validate width (type & value) """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ retrieve height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height : set and validate height (type & value) """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ returns current rectangle area """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ returns current rectangle area """
+        return (self.__height + self.width) * 2
