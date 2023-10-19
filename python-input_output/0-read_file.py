773d8788bf52704b284da6973ec93010aef53e30
root <enidaucl@gmail.com>
2023-10-19 13:55:42 -0700

    read a file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" read a file(UTF8) and prints it to stdout """
+
+
+def read_file(filename=""):
+    """ read  and print a file """
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = file.read()
+        print(content, "")
