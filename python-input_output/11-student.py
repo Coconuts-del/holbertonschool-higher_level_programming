aaeb42cc9d66172e3e8655ba2795577df71c4d20
root <enidaucl@gmail.com>
2023-10-20 10:41:22 -0700

    Student to disk and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Student module
+"""
+
+
+class Student:
+    """ Student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        """ retrives common attributes to the list and the dict """
+        new_dict = {}
+        for attr in self.__dict__.keys():
+            if attr in attrs:
+                new_dict[attr] = self.__dict__[attr]
+        return new_dict
+
+    def reload_from_json(self, json):
+        """ replace all attributes reload from json """
+        if not json:
+            return
+        for attr in self.__dict__:
+            if attr in self.__dict__:
+                self.__dict__[attr] = json[attr]
