7a0afeaa57772d99221e42fc311d9591f70bc224
root <enidaucl@gmail.com>
2023-10-20 07:47:57 -0700

    dict class

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+    returns the dictionnary description with simpl data structure
+"""
+
+
+def class_to_json(obj):
+    """ Dictionary description """
+    return obj.__dict__
