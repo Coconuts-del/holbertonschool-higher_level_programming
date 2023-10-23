e29f76ececaa03d7e9c0482b23998505dddd083a
root <enidaucl@gmail.com>
2023-10-23 13:37:44 -0700

    base.py

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    defines a class
+"""
+
+
+class Base:
+    """ create:q: class named base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ initialize a new Base """
+        if id is not None:
+            self.id = id
+        else:
+            base.__nb_objects += 1
+            self.id = Base.__nb_objects
