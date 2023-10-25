05de946462a654fa1958d998ede2a20afc935b4c
root <enidaucl@gmail.com>
2023-10-25 08:08:34 -0700

    JSON string to dictionary

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -41,5 +41,5 @@ class Base:
     def from_json_string(json_string):
         """ return the list of the JSON string representation json_string """
         if json_string is None:
-            return "[]"
+            return []
         return json.loads(json_string)
