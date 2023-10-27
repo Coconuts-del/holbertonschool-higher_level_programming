06870d65b823f875e88e285e8e80e069ff4f5545
Coconuts-del <6712@holbertonstudents.com>
2023-10-27 18:05:40 +0200

    tests base

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -3,32 +3,34 @@
 unittest for class Base
 """
 import unittest
-import json
 from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
 
 
 class TestBaseClass(unittest.TestCase):
     """ tests Base """
+    def test_id(self):
+        self.assertEqual(Base(5).id, 5)
+        self.assertEqual(Base().id, 1)
+        self.assertEqual(Base().id, 2)
+        self.assertEqual(Base(180).id, 180)
+    
+    def test_to_json_string_none(self):
+        self.assertEqual(Base.to_json_string(None), '[]')
 
+    def test_to_json_string_empty(self):
+        self.assertEqual(Base.to_json_string([]), '[]')
 
-    def test_init_with_id(self):
-        b = Base(5)
-        self.assertEqual(b.id, 5)
+    def test_to_json_string_exists(self):
+        self.assertEqual(Base.to_json_string([{'id': 80}]), '[{"id": 80}]')
 
-    def test_init_without_id(self):
-        b1 = Base()
-        b2 = Base()
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
+    def test_from_json_string_none(self):
+        self.assertEqual(Base.from_json_string(None), [])
 
-    def test_to_json_value(self):
-        sq = Square(10, 5, 5, 50)
-        js_dict = sq.to_dictionary()
-        js_str = Base.to_json_string([js_dict])
-        self.assertEqual(
-            json.loads(js_str), [{"id": 50, "y": 5, "size": 10, "x": 5}])
+    def test_from_json_string_empty(self):
+        self.assertEqual(Base.from_json_string("[]"), [])
 
+    def test_from_json_string_exists(self):
+        self.assertEqual(Base.from_json_string('[{ "id": 52}]'), [{'id': 52}])
+        
 if __name__ == '__main__':
     unittest.main()
