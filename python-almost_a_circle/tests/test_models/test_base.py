089a233d57db77107fdcc31ec5d3b88eb58d7e32
Coconuts-del <6712@holbertonstudents.com>
2023-10-27 17:29:48 +0200

    tests base

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -3,18 +3,32 @@
 unittest for class Base
 """
 import unittest
+import json
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
-class TestBase(unittest.TestCase):
-    """ test if id is correct """
-    def test_init(self):
-        b = Base()
-        self.assertEqual(b.id, 1)
+class TestBaseClass(unittest.TestCase):
+    """ tests Base """
 
-        b = Base(15)
-        self.assertEqual(b.id, 15)
 
+    def test_init_with_id(self):
+        b = Base(5)
+        self.assertEqual(b.id, 5)
+
+    def test_init_without_id(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+
+    def test_to_json_value(self):
+        sq = Square(10, 5, 5, 50)
+        js_dict = sq.to_dictionary()
+        js_str = Base.to_json_string([js_dict])
+        self.assertEqual(
+            json.loads(js_str), [{"id": 50, "y": 5, "size": 10, "x": 5}])
 
 if __name__ == '__main__':
     unittest.main()
