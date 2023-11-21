38216b3f95fd70876ec504a1cf48a53accb24ae1
Coconuts-del <6712@holbertonstudents.com>
2023-11-21 15:42:56 +0100

    creates the MySQL server user user_0d_1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- creates the user user_0d_1 with all privileges and password set to user_0d_1_pwd 
+-- dont failed if user_0d_1 already: exists
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
