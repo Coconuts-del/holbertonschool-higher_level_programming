5a2815fc8f5f8e20c140704a656b4e233aabe1b7
Coconuts-del <6712@holbertonstudents.com>
2023-11-22 18:57:52 +0100

    fix syntax in number of shows by genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 --  and display the number of shows linked to each  using only 1 select
 --  sorted descending order by number of shows linked  
 --  database name pass as an argument of mysql command 
-SELECT tv_genres.name as genre, count(*) as number_of_shows 
+SELECT tv_genres.name AS genre, count(*) AS number_of_shows 
   FROM tv_genres 
   JOIN tv_show_genres
   ON tv_genres.id = tv_show_genres.genre_id 
